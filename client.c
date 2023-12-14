e556f4ee9bd0a2d344b6c9a8e2fcbde66b5a4ff5
inbarweissler <inbar.frenkel@gmail.com>
2023-12-15 11:20:15 +1300

    first commit

---
+++ client.c
@@ -0,0 +1,32 @@
+/*
+ * File: client.c
+ * Author: Inbar R. Weissler
+ * Created on: December 15, 2023
+ * Description:
+ */
+
+
+#include <stdio.h>
+
+
+/******************
+ * Global Constants
+ ******************/
+#define PORT_NUM 1908
+
+
+/****************
+ * Function Declarations
+ ****************/
+
+/*
+ * Placeholder for receiving a new file
+ * Parameters:
+ * Returns:
+ */
+
+
+int main(){
+    
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,32 @@
+/*
+ * File: server.c
+ * Author: Inbar R. Weissler
+ * Created on: December 15, 2023
+ * Description:
+ */
+
+
+#include <stdio.h>
+
+
+/******************
+ * Global Constants
+ ******************/
+#define PORT_NUM 1908
+
+
+/****************
+ * Function Declarations
+ ****************/
+
+/*
+ * Placeholder for handling client
+ * Parameters:
+ * Returns:
+ */
+
+
+int main(){
+    
+    return 0;
+}
